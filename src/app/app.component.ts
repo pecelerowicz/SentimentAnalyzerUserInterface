a3533b3bbb550c5803a2f4bca9deb7dc0de948c6
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2024-06-24 11:12:13 +0200

    test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'sentiment-analyzer-user-interface';
+  title = 'sentiment-analyzer-user-interface-test';
 }
